85c7e151de829536f1504b1da2dc0cc9e4422955
Jerry Chapman <raptor90se@msn.com>
2019-11-22 19:56:11 -0700

    Added sensor_readings.cpp and .h

--- src/main.cpp
+++ src/main.cpp
@@ -35,37 +35,3 @@ void loop() {
   delay(2000);
 }
 
-void refresh_readings() {
-  float f_temperature;
-  float f_humidity;
-  float f_pressure;
-  float f_altitude;
-
-  digitalWrite(LED_BUILTIN, HIGH);
-
-  f_temperature = bme.readTemperature();
-  f_humidity = bme.readHumidity();
-  f_pressure = bme.readPressure() / 100.0F;
-  f_altitude = bme.readAltitude(SEALEVELPRESSURE_HPA);
-
-  // Temperature
-  f_temperature = (f_temperature * 9/5) + 32;
-  Serial.print(f_temperature);
-  Serial.println(" °F");
-
-  // Humidity
-  Serial.print(f_humidity);
-  Serial.println(" %");
-
-  // Pressure
-  Serial.print(f_pressure);
-  Serial.println(" hPa");
-
-  // Appx altitude
-  f_altitude = f_altitude * 3.28;
-  Serial.print(f_altitude);
-  Serial.println(" feet");   
-  
-  digitalWrite(LED_BUILTIN, LOW);
-  Serial.println("------------");   
-}
